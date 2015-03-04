8c95991287f50213cd2405eb8dffd4b3860657c3
Dearth <this@that.com>
2015-03-04 14:57:55 -0500

    finder now uses multiple cores.

--- prime/finder.go
+++ prime/finder.go
@@ -2,34 +2,54 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"os"
+	"strconv"
 )
 
 
-func find_prime(num int) {
+func find_prime(num int, tf chan int) {
 	
 	for i := 2; i < num; i++ {
 		
 		if num%i == 0 {
-			return
+			tf <- 0
+			return 
 		}
 	}
-
-	fmt.Printf("%d is prime\n", num)
+	tf <- num
 }
 
-func find_all_primes(num int){
-	
+func find_all_primes(num int, tf chan int) {
+
 	for i := 2; i < num; i++ {
-		go find_prime(i)
+		go find_prime(i, tf)
 	}
 }
 
 func main() {
- 	
-	test := 100000
+	runtime.GOMAXPROCS(runtime.NumCPU())	
 
-	find_all_primes(test)
+	tf := make(chan int)
 
+	args := os.Args[1:]
+
+	limit, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Error processing number.")
+		panic(err)
+	}
+
+	go find_all_primes(limit, tf)
+
+	for i := 2; i < limit; i++ {
+		num := <-tf
+		if num != 0 {
+			fmt.Println(num, "is prime.")
+		}
+	}
+
+	
 	fmt.Printf("Jobs finished.\n")
 }
 
